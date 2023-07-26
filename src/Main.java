a99a260687f3b10fd9167aa6817b63c37e8edf2f
Beloyara <beloyara@inbox.ru>
2023-07-26 16:22:09 +0500

    Решение первой задачи

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int a = 40000;
-        System.out.println(a);
+        System.out.println("Значение переменной a с типом int равно " + a);
+        byte b = 22;
+        System.out.println("Значение переменной b с типом byte равно " + b);
+        short c = 10000;
+        System.out.println("Значение переменной c с типом short равно " + c);
+        long d = 1000000000L;
+        System.out.println("Значение переменной d с типом long равно " + d);
+        float e = 3.75f;
+        System.out.println("Значение переменной e с типом float равно " + e);
+        double f = 3.756893D;
+        System.out.println("Значение переменной f с типом double равно " + f);
     }
 }
